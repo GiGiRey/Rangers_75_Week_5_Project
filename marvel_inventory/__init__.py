6d45380495037ffc65c98475f63b3db193791b66
GiGiRey <86616436+GiGiRey@users.noreply.github.com>
2021-11-11 15:40:25 -0600

    Day 2 updates

--- marvel_inventory/__init__.py
+++ marvel_inventory/__init__.py
@@ -4,10 +4,26 @@ from .site.routes import site
 from .authentication.routes import auth
 from config import Config
 
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_cors import CORS
+
+from .models import db as root_db
+
 app = Flask(__name__)
+
+
 
 app.register_blueprint(api)
 app.register_blueprint(site)
 app.register_blueprint(auth)
 
 app.config.from_object(Config)
+
+root_db.init_app(app)
+
+migrate = Migrate(app,root_db)
+
+app.config.from_object(Config)
+
+CORS(app)
